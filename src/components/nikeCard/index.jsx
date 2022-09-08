eaa56cade5c9cc7b904fe7a0f54528edfa7b0b7a
Imon <contact@imonweb.co.uk>
2022-09-08 16:14:02 +0100

    nike

--- src/components/nikeCard/index.jsx
+++ src/components/nikeCard/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import React from "react";
 import styled from "styled-components";
 
-const CardContainer = styled.div `
- width: 285px;
+import AirJordanImg from "../../assets/images/air-jordan-transparent.png";
+import { ShoesDetails } from "./shoesDetails";
+
+const CardWrapper = styled.div`
+  width: 100%;
+  perspective: 2000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardContainer = styled(motion.div)`
+  width: 285px;
   height: 500px;
   display: flex;
   flex-direction: column;
@@ -14,7 +26,7 @@ const CardContainer = styled.div `
   cursor: grab;
 `;
 
-const CircleWrapper = styled.div `
+const CircleWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -24,7 +36,7 @@ const CircleWrapper = styled.div `
   border-top-right-radius: 25px;
 `;
 
-const Circle = styled.div `
+const Circle = styled.div`
   position: absolute;
   width: 350px;
   height: 350px;
@@ -35,7 +47,7 @@ const Circle = styled.div `
   border-radius: 50%;
 `;
 
-const TopContainer = styled.div `
+const TopContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -44,19 +56,82 @@ const TopContainer = styled.div `
   align-items: center;
   justify-content: flex-end;
   padding: 1em 15px;
-`
+`;
 
-const BottomContainer = styled.div `
+const BottomContainer = styled.div`
   display: flex;
   flex: 0.8;
-  padding: 0 1em 0 1em;
-`
+  padding: 0 1em;
+`;
 
-export function  NikeCard(props){
-  return 
-  <CardContainer>
-    <CircleWrapper>
-      <Circle />
-    </CircleWrapper>
-  </CardContainer>
+const NikeText = styled.h1`
+  color: #fff;
+  text-transform: uppercase;
+  margin: 0;
+  z-index: 10;
+  font-size: 76px;
+  font-weight: 900;
+`;
+
+const ShoesWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Shoes = styled(motion.div)`
+  width: auto;
+  height: 190px;
+  z-index: 99;
+  user-select: none;
+  margin-right: 3em;
+  margin-top: 4em;
+
+  img {
+    width: auto;
+    height: 100%;
+    user-select: none;
+  }
+`;
+
+export function NikeCard(props) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useTransform(y, [-100, 100], [30, -30]);
+  const rotateY = useTransform(x, [-100, 100], [-30, 30]);
+
+  return (
+    <CardWrapper>
+      <CardContainer
+        style={{ x, y, rotateX, rotateY, z: 100 }}
+        drag
+        dragElastic={0.16}
+        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <TopContainer>
+          <CircleWrapper>
+            <Circle />
+          </CircleWrapper>
+          <ShoesWrapper>
+            <Shoes
+              style={{ x, y, rotateX, rotateY, rotate: "-25deg", z: 100000 }}
+              drag
+              dragElastic={0.12}
+              whileTap={{ cursor: "grabbing" }}
+            >
+              <img src={AirJordanImg} />
+            </Shoes>
+          </ShoesWrapper>
+          <NikeText>NIKE AIR</NikeText>
+        </TopContainer>
+        <BottomContainer>
+          <ShoesDetails />
+        </BottomContainer>
+      </CardContainer>
+    </CardWrapper>
+  );
 }
